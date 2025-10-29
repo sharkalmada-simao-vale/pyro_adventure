9b6d4bf6bbc959cf4cf8aed668377cb4443c789c
sharkalmada-simao-vale <shark12798@sharkcoders.pt>
2025-10-29 19:52:53 +0000

    Add files via upload

---
+++ interface.go
@@ -0,0 +1,8 @@
+components {
+  id: "interface"
+  component: "/main/interface.gui"
+  position {
+    x: -4.725093
+    y: -3.5708673
+  }
+}
---
+++ pekka.go
@@ -0,0 +1,38 @@
+components {
+  id: "pekka"
+  component: "/main/assets/scripts/pekka.script"
+}
+embedded_components {
+  id: "collisionobject"
+  type: "collisionobject"
+  data: "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"pekka\"\n"
+  "mask: \"player\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_SPHERE\n"
+  "    position {\n"
+  "    }\n"
+  "    rotation {\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 1\n"
+  "  }\n"
+  "  data: 64.32315\n"
+  "}\n"
+  ""
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"pekka\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/assets/tiles/player.atlas\"\n"
+  "}\n"
+  ""
+}
